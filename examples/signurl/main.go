b30d1e15e8f953f5a9c2af1eb4175f29236a71e1
apstndb <803393+apstndb@users.noreply.github.com>
2021-06-13 21:48:56 +0900

    Update signurl example

--- examples/signurl/main.go
+++ examples/signurl/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"cloud.google.com/go/storage"
+	"github.com/apstndb/adcplus"
 	"github.com/apstndb/adcplus/signer"
 )
 
@@ -19,13 +20,20 @@ func main() {
 }
 
 func _main() error {
-	method := flag.String("m", "GET", "")
-	duration := flag.Duration("d", 1*time.Hour, "")
+	method := flag.String("m", "GET", "HTTP method")
+	duration := flag.Duration("d", 1*time.Hour, "duration")
+	impersonateServiceAccount := flag.String("i", "", "impersonate service account")
+	jsonKeyFile := flag.String("k", "", "service account key file")
+	useSignatureVersion4 := flag.Bool("4", false, "Use signature version 4")
 	flag.Parse()
 	ctx := context.Background()
-	s, err := signer.SmartSigner(ctx)
+	s, err := signer.SmartSigner(ctx, adcplus.WithTargetPrincipal(*impersonateServiceAccount), adcplus.WithCredentialsFile(*jsonKeyFile))
 	if err != nil {
 		return err
+	}
+	var scheme storage.SigningScheme
+	if *useSignatureVersion4 {
+		scheme = storage.SigningSchemeV4
 	}
 
 	re := regexp.MustCompile(`^gs://(?P<bucket>[^/]*)/(?P<name>.*)$`)
@@ -38,6 +46,7 @@ func _main() error {
 		GoogleAccessID: s.ServiceAccount(ctx),
 		SignBytes:      signer.SignWithoutKeyAdaptor(ctx, s),
 		Method:         *method,
+		Scheme: 		scheme,
 		Expires:        time.Now().Add(*duration),
 	})
 	if err != nil {
